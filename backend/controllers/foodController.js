a1c9ffb34e282df441fd5de884d741fc62d803db
yashlokwani <yashlokwani22@gmail.com>
2025-04-10 11:35:09 +0530

    remove api add, all done!

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -40,4 +40,22 @@ export const listFood = async (req, res) => {
     }
 }
 
-export default {addFood, listFood};
+
+// delete food items
+
+export const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`, () => {})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+
+        res.json({success:true, message:"Food Removed Successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+export default {addFood, listFood, removeFood};
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import foodModel from "../models/foodModel.js"
 import multer from 'multer'
-import { addFood,listFood } from '../controllers/foodController.js'
+import { addFood,listFood,removeFood } from '../controllers/foodController.js'
 
 
 const foodRouter = express.Router();
@@ -19,6 +19,8 @@ const upload = multer({storage:storage})
 foodRouter.post("/add", upload.single("image"), addFood)
 
 foodRouter.get("/list", listFood)
+
+foodRouter.post("/remove", removeFood)
 
 
 export default foodRouter;
